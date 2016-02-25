d028a0e1e799b435d5dfc1321a1827abb23b48ad
bofiss <bofiss@gmail;com>
2016-02-25 15:58:24 +0100

    change drupal installation script

--- bin/install_drupal_mysql.sh
+++ bin/install_drupal_mysql.sh
@@ -5,9 +5,14 @@
 # 'root' with no password.  Travis sets
 # up one of these for you by default.
 #
+
 # remove html folder if exist.
 rm -rf html
+$BASEDIR\vendor\bin\drush  make -y --force-complete drupal.make html
+cd html
+$BASEDIR\vendor\bin\drush si -y standard --db-url=mysql://root@localhost/dp7 --account-name=admin --account-pass=admin
+
 
 # drush quick install
 
-drush -y core-quick-drupal --profile=standard -- makefile=drupal.make --db-url=mysql://root@localhost/cidrupal --core=drupal-7.x  devel behatrunner --browser=0 --strict=0 --no-server --root=html --account-name=admin --account-pass=admin
+#drush -y core-quick-drupal --profile=standard -- makefile=drupal.make --db-url=mysql://root@localhost/cidrupal --core=drupal-7.x  devel behatrunner --browser=0 --strict=0 --no-server --root=html --account-name=admin --account-pass=admin
