64883cf60af5888a6218d0531dcb1efc8b3f542b
bofiss <bofiss@gmail;com>
2016-02-26 09:50:07 +0100

    modified install_drupal an travis conf file

--- bin/install_drupal_mysql.sh
+++ bin/install_drupal_mysql.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# Build instance of Drupal using mysql.
+#
+# Build an instance of Drupal using mysql.
 # This assumes that you have a mysql user
 # 'root' with no password.  Travis sets
 # up one of these for you by default.
 #
-
-# remove html folder if exist.
 rm -rf html
-$BASEDIR\vendor\bin\drush  make -y --force-complete drupal.make html
+$BASEDIR/vendor/bin/drush make -y --force-complete drupal.make html
 cd html
-$BASEDIR\vendor\bin\drush si -y standard --db-url=mysql://root@localhost/cidrupal --db-su='root' --db-su-pw='' --account-name=admin --account-pass=admin
-
-
-# drush quick install
-
-#drush -y core-quick-drupal --profile=standard -- makefile=drupal.make --db-url=mysql://root@localhost/cidrupal --core=drupal-7.x  devel behatrunner --browser=0 --strict=0 --no-server --root=html --account-name=admin --account-pass=admin
+$BASEDIR/vendor/bin/drush si -y standard --db-url=mysql://root@localhost/cidrupal --account-name=admin --account-pass=admin
