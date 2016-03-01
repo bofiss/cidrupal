efbb6b37727638ef2defe5170a6ed0bb63674993
bofiss <bofiss@gmail;com>
2016-03-01 15:43:17 +0100

    detect os, and changing phpunit conf file

--- phpunit.drupal_bootstrap.php
+++ phpunit.drupal_bootstrap.php
@@ -11,7 +11,7 @@
 
 // set here drupal root path
 $drupal_root = guessDrupalRootPath();
-
+print_r($drupal_root);
 // in a multisite installation, set here the domain name you are testing.
 $http_host = 'localhost';
 
@@ -22,8 +22,15 @@ $http_host = 'localhost';
 // try to guess drupal root path if not specified.
 function guessDrupalRootPath() {
   $path = getcwd();
+  $os =strtoupper(substr(PHP_OS, 0, 3));
+
+  if ($os == "WIN"){
+    $array = explode("\\", $path);
+    $path = implode('/', $array);
+  }
+
   while ($path != '/') {
-    if (file_exists($path . '/includes/bootstrap.inc')) {
+    if (file_exists($path . '/html/includes/bootstrap.inc')) {
       break;
     }
     $path = dirname($path);
@@ -32,13 +39,13 @@ function guessDrupalRootPath() {
 }
 
 $_SERVER['HTTP_HOST'] = $http_host;
-$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_NAME'] = '/html/index.php';
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 $_SERVER['REQUEST_METHOD'] = 'GET';
 $_SERVER['SERVER_NAME'] = NULL;
 $_SERVER['SERVER_SOFTWARE'] = NULL;
 $_SERVER['HTTP_USER_AGENT'] = NULL;
 
-define('DRUPAL_ROOT', $drupal_root);
+define('DRUPAL_ROOT', $drupal_root.'/html');
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
---
+++ tests/HelloWorldtest.php
@@ -0,0 +1,5 @@
+<?php
+
+class HelloWorldTest extends PHPUnit_Framework_TestCase {
+
+}
