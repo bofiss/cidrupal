79b0ffcc2d0ea40923564d4712c35c1f4b95db73
U-ACTENCY\vbelle <vbelle@PC-SG-019.actency.local>
2016-04-29 15:31:16 +0200

    romve loader

--- tests/firstTest.php
+++ tests/firstTest.php
@@ -1,9 +1,5 @@
 <?php
-define('DRUPAL_ROOT', getcwd());
-require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-// Bootstrap Drupal.
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
 
 class FirstTest extends PHPUnit_Framework_TestCase{
 
