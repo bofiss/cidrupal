8759d9a31e051a707aa81cc6c28c8261c5c4ab0b
U-ACTENCY\vbelle <vbelle@PC-SG-019.actency.local>
2016-04-29 14:44:43 +0200

    change hello world test

--- tests/HelloWorldtest.php
+++ tests/HelloWorldtest.php
@@ -5,7 +5,7 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase {
 
  public function testHelloWorld_Help(){
 
-   $this->assertEquals(check_plain("test"), "test");
+   $this->assertEquals(check_plain("testo"), "testo");
    $this->assertEquals("<p>Hello world help.</p>", helloworld_help("admin/help#helloworld") );
    print_r(_dblog_get_message_types());
  }
