42f97f0ce7f298269db278eebcbb8d2e116ac546
U-ACTENCY\vbelle <vbelle@PC-SG-019.actency.local>
2016-04-29 15:25:46 +0200

    other tests

--- tests/firstTest.php
+++ tests/firstTest.php
@@ -8,8 +8,8 @@ drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 class FirstTest extends PHPUnit_Framework_TestCase{
 
   public function testLoginChk(){
-    print_r(user_authenticate('admn', 'admin'));
-    $this->assertEquals(0, user_authenticate('admin', 'admin'));
+    print_r(user_authenticate('admin', 'admin'));
+    $this->assertEquals(0, user_authenticate('admn', 'admin'));
   }
 
 }
