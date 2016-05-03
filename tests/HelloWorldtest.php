1d53344278ea016590871e79e78f1940a32940b8
bofiss <bofiss@gmail.com>
2016-05-03 08:50:12 +0000

    test help message from helloworld module

--- tests/HelloWorldtest.php
+++ tests/HelloWorldtest.php
@@ -1,4 +1,9 @@
 <?php
+define('DRUPAL_ROOT', getcwd());
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+// Bootstrap Drupal.
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 class HelloWorldTest extends PHPUnit_Framework_TestCase {
 
--- tests/firstTest.php
+++ tests/firstTest.php
@@ -6,6 +6,9 @@ class FirstTest extends PHPUnit_Framework_TestCase{
   public function testLoginChk(){
     print_r(user_authenticate('admin', 'admin'));
     $this->assertEquals(1, user_authenticate('admin', 'admin'));
+    $this->assertEquals(check_plain("test"), "test");
+    $this->assertEquals("<p>Hello world help.</p>", helloworld_help("admin/help#helloworld") );
+    print_r(_dblog_get_message_types());
 
   }
 
