ef59045e8b95f323002095ad9ef3b036e9aacb0b
bofiss <bofiss@gmail.com>
2016-05-03 08:44:26 +0000

    fix test

--- tests/firstTest.php
+++ tests/firstTest.php
@@ -5,7 +5,7 @@ class FirstTest extends PHPUnit_Framework_TestCase{
 
   public function testLoginChk(){
     print_r(user_authenticate('admin', 'admin'));
-    $this->assertEquals(1, user_authenticate('admi', 'admin'));
+    $this->assertEquals(1, user_authenticate('admin', 'admin'));
 
   }
 
