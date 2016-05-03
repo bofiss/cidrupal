3222bd7ca6c026b76d2486067d33ce3de09ed127
bofiss <bofiss@gmail.com>
2016-05-03 11:20:05 +0000

    add hello world new test

--- tests/HelloWorldtest.php
+++ tests/HelloWorldtest.php
@@ -1,14 +1,10 @@
 <?php
-define('DRUPAL_ROOT', getcwd());
-require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-// Bootstrap Drupal.
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
 
 class HelloWorldTest extends PHPUnit_Framework_TestCase {
 
 
- public function testHelloWorld_Help(){
+ public function testHelloWorld(){
 
    $this->assertEquals(check_plain("test"), "test");
    $this->assertEquals("<p>Hello world help.</p>", helloworld_help("admin/help#helloworld") );
