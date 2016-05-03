88f438e520d86ea35a409a8a8afddd9efa778b8d
bofiss <bofiss@gmail.com>
2016-05-03 13:18:30 +0000

    insert test for tdd module test

--- bin/drush.bat
+++ bin/drush.bat
@@ -1,7 +1,1 @@
-@ECHO OFF
-<<<<<<< HEAD
-SET BIN_TARGET=%~dp0/../vendor/drush/drush/drush
-=======
-SET BIN_TARGET=%~dp0/../vendor/bin/drush/drush/drush
->>>>>>> 7bcae3dbd7620b15e30bc4c97aed4bb0c191de65
-php "%BIN_TARGET%" %*
+../vendor/drush/drush/drush.bat
