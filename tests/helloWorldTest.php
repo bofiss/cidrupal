71742b02fddb96eab8bcc3e544251f494f94056d
Victor Belle-belle <vbelle@actency.local>
2016-05-04 11:55:23 +0200

    fix test

--- tests/helloWorldTest.php
+++ tests/helloWorldTest.php
@@ -6,7 +6,7 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase {
 
  public function testHelloWorld(){
 
-   $this->assertEquals(check_plain("test"), "test");
+   $this->assertEquals(check_plain("tests"), "tests");
    $this->assertEquals("<p>Hello world help.</p>", helloworld_help("admin/help#helloworld") );
    print_r(_dblog_get_message_types());
  }
